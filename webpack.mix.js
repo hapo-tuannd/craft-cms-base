dc6a7e79321f1b716c8c2145397dda4a05f89bac
hapo-vinhdq <vinhdq@haposoft.com>
2020-08-28 14:06:24 +0700

    add boostrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,15 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.postCss('resources/css/app.css', 'web/dist/css/app.css', [
-        tailwindcss('tailwind.config.js')
-    ]
-);
+// mix.postCss('resources/css/app.css', 'web/dist/css/app.css', [
+//         tailwindcss('tailwind.config.js')
+//     ]
+// );
+
+mix.setPublicPath('web/dist');
+mix.setResourceRoot('./');
+
+// JS compile
+mix.js('resources/js/app.js', 'web/dist/js').version();
+// SCSS compile
+mix.sass('resources/sass/app.scss', 'web/dist/css').version();
